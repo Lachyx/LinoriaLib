d3123d520058104a6ed01389a7fa167f6c90b0cf
Lachy <lachyxx@proton.me>
2024-07-15 11:51:55 +1000

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,7 +1,4 @@
--- New example script written by wally
--- You can suggest changes with a pull request or something
-
-local repo = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/'
+local repo = 'https://raw.githubusercontent.com/Lachyx/LinoriaLib/main/'
 
 local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
 local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
